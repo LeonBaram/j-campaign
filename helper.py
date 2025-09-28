c2e1036f9aab7139f4f475f652408ffb9d1de61b
Leon Baram <leon@localhost.localdomain>
2025-09-28 17:06:07 -0400

    (Re)create character sheet as yaml

---
+++ helper.py
@@ -0,0 +1,21 @@
+import re
+
+
+def counter(curr, max=None, *reset):
+    curr = curr if curr else "0 / 0"
+    a, b = [x.strip() for x in curr.split('/')]
+    max = max if max else b
+    val = max if any(reset) else a
+    return f'{val} / {max}'
+
+
+def weight(item):
+    pattern = r'(\d+x )?([\d.]+lb )?.+'
+    match = re.fullmatch(pattern, item)
+    assert bool(match)
+    q, w = match.groups()
+    q = q if q else "1"
+    w = w if w else "0"
+    q = float(q.strip(" x"))
+    w = float(w.strip(" lb"))
+    return q * w
