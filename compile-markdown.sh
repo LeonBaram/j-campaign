f2b28c1c05e870ad4ba293d9b3f1fb25c04c1a33
Leon Baram <leon@localhost.localdomain>
2025-10-06 22:09:44 -0400

    Add yeeval step to compile-markdown script

--- compile-markdown.sh
+++ compile-markdown.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-mustache ./elbar.yml ./elbar-rockseeker.template.md > ./elbar-rockseeker.md
+yeeval ./elbar.yml && \
+    mustache ./elbar.yml ./elbar-rockseeker.template.md > ./elbar-rockseeker.md
